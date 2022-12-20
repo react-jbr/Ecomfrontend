e12137aedbf1afc1bd14fca7482cd9cf2f85b221
jabeermk <jabeermk@gmail.com>
2022-12-20 22:52:22 +0530

    init

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import Product from "../components/Product.js";
+import MessageBox from "../components/MessageBox.js";
+import LoadingBox from "../components/LoadingBox.js";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions.js";
+
+export default function HomeScreen() {
+  const dispatch = useDispatch();
+
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <div>
+     {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <>
+          {products.length === 0 && <MessageBox>No Product Found</MessageBox>}
+          <div className="row center">
+            {products.map((product) => (
+              <Product key={product._id} product={product}></Product>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
